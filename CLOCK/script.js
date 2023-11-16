d12465b0392af9b47ca03ba8be06a72cfb990146
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2023-11-16 13:41:23 +0700

    fix

--- CLOCK/script.js
+++ CLOCK/script.js
@@ -24,24 +24,25 @@ function dongho()
 
 
 
-// Hẹn giờ
+
 var timer;
 function setTimer() {
-    //lấy các giá tị ở các phần thử id bên HTML
+
     var hours = document.getElementById('hours').value;
     var minutes = document.getElementById('minutes').value;
     var seconds = document.getElementById('seconds').value;
 
 
-    var currentTime = new Date(); // thời gian hiện tại
-    var targetTime = new Date();//
-    targetTime.setHours(hours);//thiết lập giá trị giờ của người nhập 
-    targetTime.setMinutes(minutes);//giá trị phút của người nhập
-    targetTime.setSeconds(seconds);//giá trị giây của người nhập 
+    var currentTime = new Date(); 
+    var targetTime = new Date();
+    targetTime.setHours(hours); 
+    targetTime.setMinutes(minutes);
+    targetTime.setSeconds(seconds);
 
     var timeDifference = targetTime.getTime() - currentTime.getTime();
 
     if (timeDifference > 0) {
+        timer = setTimeout(showTime, timeDifference);
         alert('Hẹn giờ đã được đặt.');
     } else {
         alert('Vui lòng chọn một thời gian hợp lệ trong tương lai.');
@@ -66,12 +67,12 @@ function showTime() {
 
     document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' +seconds;
 
-    timer = setTimeout(showTime, 1000); // Cập nhật thời gian mỗi giây
+    timer = setTimeout(showTime, 1000); 
 
-    // Kiểm tra nếu đến thời gian hẹn giờ
-    var targetHours = document.getElementById('hours').value;
-    var targetMinutes = document.getElementById('minutes').value;
-    var targetSeconds = document.getElementById('seconds').value;
+    
+    var targetHours = parseInt(document.getElementById('hours').value);
+    var targetMinutes = parseInt(document.getElementById('minutes').value);
+    var targetSeconds = parseInt(document.getElementById('seconds').value);
 
     if (hours == targetHours && minutes == targetMinutes && seconds == targetSeconds) {
         alert('Đã đến thời gian hẹn giờ!');
@@ -85,7 +86,5 @@ function formatTime(time) {
     }
     return time;
 }
-
-
 
 
